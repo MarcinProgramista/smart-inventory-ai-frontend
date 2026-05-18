cd39489a54690eeaee13cecfbc5723f4bc3ba6cc
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-18 18:11:54 +0200

    Setup app routing and add start page card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import StartPageCard from "./components/ui/StartPageCard";
+
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<StartPageCard />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
