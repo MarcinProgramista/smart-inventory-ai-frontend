4d96c144fc05f7f788dc200785ca0a4ebacabdc7
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-21 10:46:08 +0200

    Implement password validation helper component

--- src/components/form/PasswordField.jsx
+++ src/components/form/PasswordField.jsx
@@ -1,19 +1,30 @@
-import { faChain, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChain,
+  faInfoCircle,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import LabelWrapper from "../ui/LabelWrapper";
 import StyledFontAwesomeIconHidePassword from "../ui/password/StyledFontAwesomeIconHidePassword";
 import StyledFontAwesomeIconInvalidPassword from "../ui/password/StyledFontAwesomeIconInvalidPassword";
 import Input from "../common/Input";
+import ParagraphPassword from "../ui/password/ParagraphPassword";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PasswordField = ({ validPwd, pwd, setPwd, setPwdFocus, pwdFocus }) => {
   return (
-    <LabelWrapper htmlFor="password">
-      Password:
-      <StyledFontAwesomeIconHidePassword icon={faChain} $validPwd={validPwd} />
-      <StyledFontAwesomeIconInvalidPassword
-        icon={faTimes}
-        $validPwd={validPwd}
-        $pwd={pwd}
-      />
+    <>
+      <LabelWrapper htmlFor="password">
+        Password:
+        <StyledFontAwesomeIconHidePassword
+          icon={faChain}
+          $validPwd={validPwd}
+        />
+        <StyledFontAwesomeIconInvalidPassword
+          icon={faTimes}
+          $validPwd={validPwd}
+          $pwd={pwd}
+        />
+      </LabelWrapper>
       <Input
         type="password"
         id="password"
@@ -26,7 +37,20 @@ const PasswordField = ({ validPwd, pwd, setPwd, setPwdFocus, pwdFocus }) => {
         onBlur={() => setPwdFocus(false)}
         placeholder="Put password .."
       />
-    </LabelWrapper>
+      <ParagraphPassword id="pwdnote" $pwdFocus={pwdFocus} $validPwd={validPwd}>
+        <FontAwesomeIcon icon={faInfoCircle} />
+        8 to 24 characters.
+        <br />
+        Must include uppercase and lowercase letters, a number and a special
+        character.
+        <br />
+        Allowed special characters: <span aria-label="exclamation mark">!</span>
+        <span aria-label="at symbol">@</span>{" "}
+        <span aria-label="hashtag">#</span>{" "}
+        <span aria-label="dollar sign">$</span>{" "}
+        <span aria-label="percent">%</span>
+      </ParagraphPassword>
+    </>
   );
 };
 
