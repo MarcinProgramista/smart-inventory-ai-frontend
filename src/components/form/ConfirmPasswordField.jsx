909f6a421350609a8c7898b56eb461359d62a12d
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-21 15:15:19 +0200

    Implement confirm password input and validation icons

--- src/components/form/ConfirmPasswordField.jsx
+++ src/components/form/ConfirmPasswordField.jsx
@@ -1,6 +1,8 @@
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faChain, faTimes } from "@fortawesome/free-solid-svg-icons";
 import LabelWrapper from "../ui/LabelWrapper";
 import StyledFontAwesomeIconHidePasswordConfirm from "../ui/confirmPassword/StyledFontAwesomeIconHidePasswordConfirm";
+import StyledFontAwesomeIconInvalidPasswordConfirm from "../ui/confirmPassword/StyledFontAwesomeIconInvalidPasswordConfirm";
+import Input from "../common/Input";
 const ConfirmPasswordField = ({
   validMatch,
   matchPwd,
@@ -13,11 +15,28 @@ const ConfirmPasswordField = ({
       <LabelWrapper htmlFor="confirm_pwd">
         Confirm Password:
         <StyledFontAwesomeIconHidePasswordConfirm
-          icon={faCheck}
+          icon={faChain}
+          $validMatch={validMatch}
+          $matchPwd={matchPwd}
+        />
+        <StyledFontAwesomeIconInvalidPasswordConfirm
+          icon={faTimes}
           $validMatch={validMatch}
           $matchPwd={matchPwd}
         />
       </LabelWrapper>
+      <Input
+        type="password"
+        id="confirm_pwd"
+        onChange={(e) => setMatchPwd(e.target.value)}
+        value={matchPwd}
+        required
+        aria-describedby="confirmnote"
+        aria-invalid={validMatch ? "false" : "true"}
+        onFocus={() => setMatchFocus(true)}
+        onBlur={() => setMatchFocus(false)}
+        placeholder="Put the same password like above.."
+      />
     </>
   );
 };
