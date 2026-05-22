df7bfb55b39115151fe3a307a3eba7fe2fe4cae1
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-22 13:05:36 +0200

    feat(auth): create login page and register /login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
