a91b0c846b6da0423d33dec729665f85290ba320
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-22 13:50:48 +0200

    feat(login): add login page UI structure and state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useRef, useState } from "react";
+import NeonUserIcon from "../components/icons/NeonUserIcon";
+import ButtonRow from "../components/layout/ButtonRow";
+import Logo from "../components/ui/Logo";
+import NeonCard from "../components/ui/NeonCard";
+import ParagraphError from "../components/ui/typography/ParagraphError";
+import Title from "../components/ui/typography/Title";
+
 const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [errMsg, setErrMsg] = useState("");
   return (
     <>
-      <h1>Log in</h1>
+      <NeonCard>
+        <ButtonRow>
+          <NeonUserIcon />
+          <Logo>SmartInventoryAI</Logo>
+        </ButtonRow>
+        <ParagraphError ref={errRef} $errMsg={errMsg} aria-live="assertive">
+          {errMsg}
+        </ParagraphError>
+        <Title>Log in</Title>
+      </NeonCard>
     </>
   );
 };
