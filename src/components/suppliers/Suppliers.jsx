a13d42a7c4af303a85fc109d99dbaedaecbe2d62
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-06-01 12:12:12 +0200

    feat: add supplier search and data fetching

--- src/components/suppliers/Suppliers.jsx
+++ src/components/suppliers/Suppliers.jsx
@@ -1,10 +1,43 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useSearchParams } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import { useContext, useEffect, useState } from "react";
+import ToastContext from "../../context/ToastContext";
+import useSupplierActions from "../../hooks/useSupplierAction";
+import useFetchSuppliers from "../../hooks/useFetchSuppliers";
+import useDebounce from "../../hooks/useDebounce";
 
 export default function Suppliers() {
   const { auth } = useAuth();
+  const { showToast } = useContext(ToastContext);
+
   const [searchParams, setSearchParams] = useSearchParams();
+  const { suppliers, total, fetchSuppliers } = useFetchSuppliers();
+
+  const query = searchParams.get("q") ?? "";
+  const page = Number(searchParams.get("page") ?? 1);
+  const limit = 8;
+  const sortBy = searchParams.get("sort") ?? "name";
+  const sortOrder = searchParams.get("order") ?? "asc";
+  const { addSupplier, updateSupplier, deleteSupplier } = useSupplierActions({
+    showToast,
+  });
+
+  const [search, setSearch] = useState(query);
+  const debouncedSearch = useDebounce(search, 400);
+
+  useEffect(() => {
+    if (!auth.id) return;
+    fetchSuppliers(auth.id, {
+      q: debouncedSearch,
+      page,
+      limit,
+      sort: sortBy,
+      order: sortOrder,
+    });
+  }, [auth?.id, debouncedSearch, page, sortBy, sortOrder]);
+  console.log(suppliers);
 
   return <h1>{auth.name}</h1>;
 }
